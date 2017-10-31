ac025aa0ada4f48f7fe42aae5719a9094c8dac03
Osamu Takiya <takiya@toran.sakura.ne.jp>
2017-10-31 23:35:25 +0900

    [add] first commit
    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# find-layout-using-specific-field
-Find layout name using specific field in FileMaker
+# What's this?
+- Find layout name using specific field in FileMaker
+
+# required gem
+- [filemaker](https://github.com/mech/filemaker-ruby)
+
+# install gem
+
+```bash
+$ bundle install --path vendor/bundle
+```
+
+# write config.yml
+- check `config.yml` as below
+
+```
+fm_server:
+  host: '123.123.123.123'
+  username: 'my_username'
+  password: 'my_password'
+  db_name: 'target_db_name'
+target_field_name: birthday
+```
+
+# exec script
+
+```bash
+$ ruby find_layout_using_specific_field.rb
+```
+
+# result
+
+```bash
+$ ruby find_layout_using_specific_field.rb
+layout_01
+layout_03
+layout_07
+layout_10
+...
+```
+
+# LICENSE
+- [MIT License](/LICENSE)
---
+++ find_layout_using_specific_field.rb
@@ -0,0 +1,23 @@
+require 'bundler/setup'
+require 'filemaker'
+
+config_file = YAML.load_file('config.yml')
+fm_server = Filemaker::Server.new do |config|
+  config.host         = config_file['fm_server']['host']
+  config.account_name = config_file['fm_server']['username']
+  config.password     = config_file['fm_server']['password']
+end
+
+db_name           = config_file['fm_server']['db_name']
+target_field_name = config_file['target_field_name']
+layout_names      = fm_server.database[db_name].layouts.all # Array
+
+layout_names.each do |layout_name|
+  begin
+    fm_server.database[db_name].layout[layout_name].view.fields.each do |field_name, field_value|
+      puts "#{layout_name}" if field_name == target_field_name
+    end
+  rescue => exception
+    # for `Layout is missing`
+  end
+end
