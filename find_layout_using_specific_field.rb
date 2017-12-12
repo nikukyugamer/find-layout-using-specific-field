b00b00f2e446b4d3707ecef4fc824fccb60f4bac
Osamu Takiya <takiya.amiami@gmail.com>
2017-12-12 12:47:39 +0900

    [update] create class
    until now without class, so create FindLayoutUsingSpecificField class

--- find_layout_using_specific_field.rb
+++ find_layout_using_specific_field.rb
@@ -1,33 +1,35 @@
 require 'filemaker'
 require 'yaml'
 
-CONFIG_FILE = YAML.load_file('config.yml')
+class FindLayoutUsingSpecificField
+  CONFIG_FILE = YAML.load_file('config.yml')
 
-def fm_server
-  fm_server = Filemaker::Server.new do |config|
-    config.host         = CONFIG_FILE['fm_server']['host']
-    config.account_name = CONFIG_FILE['fm_server']['username']
-    config.password     = CONFIG_FILE['fm_server']['password']
+  def fm_server
+    fm_server = Filemaker::Server.new do |config|
+      config.host         = CONFIG_FILE['fm_server']['host']
+      config.account_name = CONFIG_FILE['fm_server']['username']
+      config.password     = CONFIG_FILE['fm_server']['password']
+    end
   end
-end
 
-def initialize
-  @target_field_name  = CONFIG_FILE['target_field_name']
-  @db_name            = CONFIG_FILE['fm_server']['db_name']
-  @layout_names       = fm_server.database[@db_name].layouts.all # Array
-end
+  def initialize
+    @target_field_name  = CONFIG_FILE['target_field_name']
+    @db_name            = CONFIG_FILE['fm_server']['db_name']
+    @layout_names       = fm_server.database[@db_name].layouts.all # Array
+  end
 
-def layout_name_using_specific_field
-  @layout_names.each do |layout_name|
-    begin
-      fm_server.database[@db_name].layout[layout_name].view.fields.each do |field_name, field_value|
-        puts "#{layout_name}" if field_name == @target_field_name
+  def layout_name_using_specific_field
+    @layout_names.each do |layout_name|
+      begin
+        fm_server.database[@db_name].layout[layout_name].view.fields.each do |field_name, field_value|
+          puts "#{layout_name}" if field_name == @target_field_name
+        end
+      rescue => exception
+        puts exception # `Layout is missing`...
       end
-    rescue => exception
-      # for `Layout is missing`
     end
   end
 end
 
-initialize
-layout_name_using_specific_field
+find_layout = new FindLayoutUsingSpecificField
+find_layout.layout_name_using_specific_field
